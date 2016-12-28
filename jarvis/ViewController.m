5f1e353aa0c79aa2199b04e475dd2ffa60b93696
Admin <admin@Mac-Admin.local>
2016-12-28 15:00:07 +0300

    init project

---
+++ jarvis/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  jarvis
+//
+//  Created by Admin on 28.12.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
